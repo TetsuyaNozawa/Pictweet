3acecbedf63babc92b61d1c0962def24ff008c71
nozawatetsuya <nozawatetsuya@team-lab.com>
2018-04-23 18:22:25 +0900

    importの作成

--- src/main/java/com/example/web/controller/TweetController.java
+++ src/main/java/com/example/web/controller/TweetController.java
@@ -1,5 +1,9 @@
 package com.example.web.controller;
 
+
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +21,8 @@ public class TweetController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mav) {
-    	Tweet tweet = tweetRepository.findOne(1L);
+    	List<Tweet> tweets = tweetRepository.findAll();
+    	mav.addObject("tweets",tweets);
         mav.setViewName("tweet/index");
         return mav;
     }
