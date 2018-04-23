638fbe8c32eb6d12430856dcc23a87726cdb5058
nozawatetsuya <nozawatetsuya@team-lab.com>
2018-04-23 17:45:24 +0900

    repository作成

--- src/main/java/com/example/web/controller/TweetController.java
+++ src/main/java/com/example/web/controller/TweetController.java
@@ -1,15 +1,23 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.business.domain.Tweet;
+import com.example.business.repository.TweetRepository;
+
 @Controller
 public class TweetController {
+	
+	@Autowired
+	private TweetRepository tweetRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mav) {
+    	Tweet tweet = tweetRepository.findOne(1L);
         mav.setViewName("tweet/index");
         return mav;
     }
